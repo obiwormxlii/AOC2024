65cce19c7cbcb5958257f87d50ba7ce4b3a2ac03
Bailey <obiwan998@gmail.com>
2024-12-05 10:11:27 -0500

    Complete day 4

--- lib/day4/day4.exs
+++ lib/day4/day4.exs
@@ -37,14 +37,81 @@ file_updates = File.read!("input4b.txt")
 
 defmodule Day4 do
   def part1(rules, updates) do
+    rules = parse_input(rules, "|")
+    updates = parse_input(updates, ",")
 
+    Enum.filter(updates, &valid_update?(&1, rules))
+    |>Enum.map(&get_middle(&1))
+    |> Enum.sum()
+    |> IO.inspect()
   end
 
-  defp parse_rules(rules) do
-    rules
-    |> String.split()
+  def part2(rules, updates) do
+    rules = parse_input(rules, "|")
+    updates = parse_input(updates, ",")
+
+    Enum.filter(updates, fn x -> not valid_update?(x, rules) end)
+    |> Enum.map(&Enum.reduce(&1, [], fn x, acc -> sort(x, acc, rules) end))
+    |>Enum.map(&get_middle(&1))
+    |> Enum.sum()
+    |> IO.inspect()
+  end
+
+  defp parse_input(input, char) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, char)
+      |> Enum.map(fn x-> String.to_integer(x) end)
+      )
+  end
+
+  defp valid_update?(update, rules) do
+    update
+    |> Enum.reduce(%{list: [], tests: []}, fn val, acc ->
+
+      %{list: acc.list ++ [val], tests: acc.tests ++ [rule_test(val, acc.list, rules)]}
+    end)
+    |> Map.get(:tests)
+    |> Enum.all?(fn x -> x == false end)
+  end
+
+  defp rule_test(val, list, rules) do
+      Enum.filter(rules, fn [x,_y] -> x == val end)
+      |> Enum.map(&Enum.member?(list, Enum.at(&1, 1)))
+      |> Enum.any?(fn x -> x == true end)
+  end
+
+  defp get_middle(list) do
+    middle_index = (length(list) - 1) |> div(2)
+    Enum.at(list, middle_index)
+  end
+
+
+  defp sort(val, acc, rules) do
+    test = rule_test(val, acc, rules)
+    if test do
+      sort(val, acc, List.pop_at(acc, -1), rules)
+    else
+      acc ++ [val]
+    end
+  end
+
+  defp sort(val, acc, pop, rules) do
+    popped_list = Enum.at(Tuple.to_list(pop), 1)
+    cond do
+      length(popped_list) == 0 ->
+        [val] ++ acc
+
+      rule_test(val, popped_list, rules) ->
+        sort(val, acc, List.pop_at(popped_list, -1), rules)
+
+      true ->
+        index = length(popped_list)
+        List.insert_at(acc, index, val)
+    end
   end
 end
 
-Day4.part1(test_rules, test_updates)
-# Day4.part1(file_rules, file_updates)
+
+# Day4.part2(test_rules, test_updates)
+Day4.part2(file_rules, file_updates)
